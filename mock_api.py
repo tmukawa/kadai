16d9112e0afb1e809f9f0ba102d2225cdc3026a3
tmukawa <tmukawa@gmail.com>
2025-03-24 22:04:19 +0900

    2025.03.24 ver03

--- mock_api.py
+++ mock_api.py
@@ -13,9 +13,9 @@ def analyze_image(request: ImageRequest):
     """ モックAPI: 画像ファイルPathを受け取って擬似的なレスポンスを返す """
     image_path = request.image_path
 
-    # 画像ファイルPathにerror文字列が含まれている場合または20%の確率でエラーを発生させる
+    # 画像ファイルPathにerror文字列が含まれている場合はエラーを発生させる
     # time.sleep(3)
-    if "error" in image_path or random.random() < 0.2:
+    if "error" in image_path:
         return {"success": False, "message": "Error:E50012", "estimated_data": {}}
     else:
         return {
